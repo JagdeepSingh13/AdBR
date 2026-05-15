bb271b27592f5396b05dae4ddb5dc096d35a82e1
JagdeepSingh13 <jagdeeps3105@gmail.com>
2026-05-15 23:57:58 +0530

    kafka and s3 config

--- video-service/src/main/java/com/adbr/videoservice/config/KafkaConfig.java
+++ video-service/src/main/java/com/adbr/videoservice/config/KafkaConfig.java
@@ -1,4 +1,30 @@
 package com.adbr.videoservice.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaConfig {
+
+//    published when video is uploaded to s3
+//    encoding service consume this
+    @Bean
+    public NewTopic videoUploadedTopic() {
+        return TopicBuilder.name("video.uploaded")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+//    published when encoding is complete
+    @Bean
+    public NewTopic videoEncodedTopic() {
+        return TopicBuilder.name("video.encoded")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
 }
