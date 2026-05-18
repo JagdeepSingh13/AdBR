eb704b8057d85e12f0526ed256d2dc8730b31def
JagdeepSingh13 <jagdeeps3105@gmail.com>
2026-05-19 03:13:47 +0530

    encoded event consumer and redis

--- streaming-service/src/main/java/com/adbr/streamingservice/config/AppConfig.java
+++ streaming-service/src/main/java/com/adbr/streamingservice/config/AppConfig.java
@@ -1,4 +1,10 @@
 package com.adbr.streamingservice.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
+    private String accessKey;
+
 }
--- streaming-service/src/main/java/com/adbr/streamingservice/service/VideoEncodedEventConsumer.java
+++ streaming-service/src/main/java/com/adbr/streamingservice/service/VideoEncodedEventConsumer.java
@@ -1,4 +1,40 @@
 package com.adbr.streamingservice.service;
 
+import com.adbr.streamingservice.event.VideoEncodedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class VideoEncodedEventConsumer {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    private static final String masterPlaylistKeyPrefix = "streaming:playlist:";
+
+//    listens to video.encoded and stores master playlist key in redis
+//    allows streaming service to quickly find playlist key by movieId
+    @KafkaListener(
+            topics = "video.encoded",
+            groupId = "streaming-service-group"
+    )
+    public void consumeVideoEncodedEvent(VideoEncodedEvent event) {
+        log.info("consumed video encoded event for: {}- {}", event.getMovieId(), event.isSuccess());
+
+        if(event.isSuccess()) {
+//            store master playlist key in redis
+            String cacheKey = masterPlaylistKeyPrefix + event.getMovieId();
+            redisTemplate.opsForValue().set(cacheKey, event.getMasterPlaylistKey());
+
+            log.info("master playlist key stored for: {}", event.getMovieId());
+        } else {
+            log.error("encoding failed for: {} - {}", event.getMovieId(), event.getErrorMessage());
+        }
+    }
+
+
 }
